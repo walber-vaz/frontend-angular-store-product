61d706422fc5c4efa26720dc622974ed26dc2c3d
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-01-24 14:58:56 -0300

    feat: update routing configuration to use APP_ROUTES and add initial routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const APP_ROUTES: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'products',
+  },
+  {
+    path: 'products',
+    loadChildren: () =>
+      import('./components/products/products.routes').then(
+        r => r.PRODUCTS_ROUTES
+      ),
+  },
+  {
+    path: 'cart',
+    loadComponent: () =>
+      import('./components/cart/cart.component').then(c => c.CartComponent),
+  },
+];
