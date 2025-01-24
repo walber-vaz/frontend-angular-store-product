fcc783d4a2a9cf73958aad90dfc93d531be3616a
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-01-24 14:59:13 -0300

    feat: add ProductsComponent with template, routing, and unit tests

---
+++ src/app/components/products/products.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { ProductsComponent } from '@/app/components/products/products.component';
+
+export const PRODUCTS_ROUTES: Routes = [
+  {
+    path: '',
+    component: ProductsComponent,
+  },
+];
