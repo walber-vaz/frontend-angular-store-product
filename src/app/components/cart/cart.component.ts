a90f55b0980cb10839df1d9cbcbdbef83ed93868
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-01-24 14:59:05 -0300

    feat: add CartComponent with template and unit tests

---
+++ src/app/components/cart/cart.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cart',
+  imports: [],
+  templateUrl: './cart.component.html',
+})
+// eslint-disable-next-line prettier/prettier
+export class CartComponent { }
