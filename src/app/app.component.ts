c56cc3218da1713540b3f6a6630d66cd5fa074ef
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-01-24 13:54:23 -0300

    feat: add LucideAngularModule to app.component imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LucideAngularModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
